a2d5a8356106ed5a745ed0e4e1b9b99f3de784de
Jakub Krejcirik <jakub.krejcirik@plus4u.net>
2019-03-23 21:39:59 +0100

    Add project files.

---
+++ KataBinary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KataBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
